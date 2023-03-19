e089aa85376cc2a333701efa1f0d1eb1a6088457
Vladislav <102545184+lezver@users.noreply.github.com>
2023-03-19 11:51:36 +0200

    finish of Task 4

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+const refs = {
+	decrementBtn: document.querySelector(`[data-action="decrement"]`),
+	incrementBtn: document.querySelector(`[data-action="increment"]`),
+	infoOfValue: document.getElementById("value"),
+	counterBox: document.getElementById("counter"),
+};
+
+const titelOfcounter = document.createElement("h1");
+titelOfcounter.classList.add("counter__title");
+titelOfcounter.textContent = "Calculator";
+refs.counterBox.prepend(titelOfcounter);
+
+let counterValue = 0;
+
+const incrementValue = () => {
+	counterValue += 1;
+	if (counterValue > 9) return (counterValue = 9);
+	refs.infoOfValue.textContent = counterValue;
+};
+
+const decrementValue = () => {
+	counterValue -= 1;
+	if (counterValue < 0) return (counterValue = 0);
+	refs.infoOfValue.textContent = counterValue;
+};
+
+refs.decrementBtn.addEventListener("click", decrementValue);
+refs.incrementBtn.addEventListener("click", incrementValue);
