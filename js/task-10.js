f8475d8c3a30a88218f2645050cfd7691bccc6d0
Vladislav <102545184+lezver@users.noreply.github.com>
2023-03-19 20:30:08 +0200

    Finished Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,37 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const refs = {
+	controls: document.querySelectorAll("#controls input, button"),
+	boxes: document.getElementById("boxes"),
+};
+
+const [amount, createBtn, destroyBtn] = refs.controls;
+
+const createBoxes = () => {
+	if (amount.value === "") alert("select a number");
+	else {
+		let arrayOfBoxes = [];
+		for (let i = 0; i < amount.value; i += 1) {
+			const box = document.createElement("div");
+			box.style.width = 30 + i * 10 + "px";
+			box.style.height = 30 + i * 10 + "px";
+			box.style.borderRadius = "5px";
+			box.style.backgroundColor = getRandomHexColor();
+
+			arrayOfBoxes.push(box);
+		}
+
+		refs.boxes.append(...arrayOfBoxes);
+	}
+};
+const desroyBoxes = () => {
+	amount.value = "";
+	refs.boxes.innerHTML = "";
+};
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", desroyBoxes);
